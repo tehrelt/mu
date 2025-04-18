e8e74f84cb777a3c0285529e6fd27a945ddfc8f6
Dmitry Evteev <haxrelt@gmail.com>
2025-04-18 14:09:07 +0300

    [chore] vite project created

--- mu-app/src/components/logout-button.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-
-import { logout } from "@/actions/auth";
-import { Button } from "./ui/button";
-
-export const LogoutButton = () => {
-  return <Button onClick={() => logout()}>Logout</Button>;
-};
