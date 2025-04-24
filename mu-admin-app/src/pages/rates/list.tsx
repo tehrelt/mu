142059b0cdcd7be7ae2ac93c69f979c1f0f3ad10
Dmitry Evteev <haxrelt@gmail.com>
2025-04-24 14:47:07 +0300

    [feat] debounced search field

--- mu-admin-app/src/pages/rates/list.tsx
+++ mu-admin-app/src/pages/rates/list.tsx
@@ -1,23 +1,41 @@
 import RateTable from "@/components/tables/rates";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { useDebounce } from "@/shared/hooks/use-debounce";
 import { routes } from "@/shared/routes";
 import { rateService } from "@/shared/services/rates.service";
 import { useQuery } from "@tanstack/react-query";
+import debounce from "lodash.debounce";
+import React from "react";
 import { Link } from "react-router-dom";
 
 export const RateListPage = () => {
+  const [search, setSearch] = React.useState("");
+  const debouncedSearch = useDebounce(search, 500);
+
   const data = useQuery({
-    queryKey: ["rates"],
-    queryFn: async () => await rateService.list(),
+    queryKey: ["rates", { query: debouncedSearch }],
+    queryFn: async () => await rateService.list({ query: debouncedSearch }),
   });
+
+  const onSearchChange = (val: string) => setSearch(val);
 
   return (
     <div className="py-8 px-12 space-y-4">
       <div className="flex justify-between items-center">
         <h1 className="text-2xl font-bold">Тарифы</h1>
-        <Link to={routes.rate.create}>
-          <Button>Создать тариф</Button>
-        </Link>
+        <div className="flex gap-x-2">
+          <div className="w-[256px]">
+            <Input
+              placeholder="Поиск"
+              value={search}
+              onChange={(e) => onSearchChange(e.target.value)}
+            />
+          </div>
+          <Link to={routes.rate.create}>
+            <Button>Создать тариф</Button>
+          </Link>
+        </div>
       </div>
       {data.isSuccess && data.data && <RateTable data={data.data.rates} />}
     </div>
--- mu-admin-app/src/shared/services/rates.service.ts
+++ mu-admin-app/src/shared/services/rates.service.ts
@@ -1,14 +1,20 @@
 import { api } from "@/app/api"
 import { RateCreate, rateListSchema, rateSchema } from "../types/rate"
 
+export type RateListRequest = {
+    query?: string
+}
+
 class RateService {
     async create(data: RateCreate) {
         const response = await api.post('/rates', data)
         return response.data
     }
 
-    async list() {
-        const response = await api.get('/rates')
+    async list(params?: RateListRequest) {
+        const response = await api.get('/rates', {
+            params
+        })
         const parsed = rateListSchema.parse(response.data)
         return parsed
     }
